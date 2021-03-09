651a03ab6989d774f9c3ebbbc158d92b84962443
Muchacho <Muchacho.ps@gmail.com>
2021-03-09 20:33:09 +0100

    initial commit

---
+++ src/app/components/library-overview/library-overview.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-library-overview',
+  template: `
+    <p>
+      library-overview works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibraryOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
